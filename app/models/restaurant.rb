f8a9abd7a70f7acf8b8aa5145d43466027e993eb
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2022-01-22 22:05:29 -0300

    validations added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
+  # validates :name, :address, :rating, :chef_name, :img presence :true 
+  validates :name, presence: true
+  validates :address, presence: true, length: {minimum: 10}
+  validates :rating, presence: true
+  validates :chef_name, presence: true
+  validates :img, presence: true, length: {minimum: 10}
 end
